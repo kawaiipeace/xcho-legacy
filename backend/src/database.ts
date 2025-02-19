7330fa4fba245e334bb2ec1ea3721a440281794a
kawaiipeace <kawaii.peace.kun@gmail.com>
2025-02-19 10:58:31 +0700

    Update database.ts

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize('xcho','postgres','123456',{
-    host: 'localhost',
-    port: 5432,
+    host: 'xcho-db', // host: localhost on bun run dev
+    port: 5432,  //port: 2502 on bun run dev
     dialect: 'postgres',
     logging: false,
 });
